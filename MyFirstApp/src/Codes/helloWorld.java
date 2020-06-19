278a448bd9568636978b5c3c1e19275538e2c304
57311 <57311@LAPTOP-M70TCKI4.lan>
2020-06-18 23:45:51 -0500

    New Commit

--- MyFirstApp/src/Codes/helloWorld.java
+++ MyFirstApp/src/Codes/helloWorld.java
@@ -3,6 +3,6 @@ package Codes;
 public class helloWorld {
 	public static void main(String args[]) {
 		System.out.println("!Hello World¡");
-		System.out.println("hjola");
+		System.out.println("dodo");
 	}
 }
